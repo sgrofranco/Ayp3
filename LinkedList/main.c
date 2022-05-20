3a7878ba4a1a5a9bf3784e7cd97b694991c72b67
Franco Sgro <65137798+sgrofranco@users.noreply.github.com>
2022-05-20 02:57:04 -0300

    Update main.c
    creacion de nodos y lsita

--- LinkedList/main.c
+++ LinkedList/main.c
@@ -1,10 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Nodo{
+typedef struct Nodo{
     int dato;
     struct Nodo* siguiente;
-};
+} Nodo;
+
+typedef struct Lista{
+    Nodo* cabeza;
+} Lista;
+
+Nodo* CrearNodo(int dato){
+    Nodo* nodo = (struct Nodo*)malloc(sizeof(struct Nodo));
+    nodo->siguiente = NULL;
+    return nodo;
+}
+
+void DestruirNodo(Nodo* nodo){
+    free(nodo);
+}
+
+void InsertarAlPrincipio(Lista* lista, int* entero){
+    Nodo* nodo = CrearNodo(entero);
+    nodo->siguiente = lista->cabeza;
+    lista->cabeza = nodo;
+}
+
+void insertarAlFinal(Lista* lista, int* entero){
+    Nodo* nodo = CrearNodo(entero);
+    if (lista->cabeza == NULL){
+        lista->cabeza = nodo;
+    } else {
+        Nodo* puntero = lista->cabeza;
+        while (puntero->siguiente){
+            puntero = puntero->siguiente;
+        }
+        puntero->siguiente=nodo;
+    }
+
+}
+
+
+void printearLista (struct Nodo* n){
+
+   while (n != NULL){
+       printf("%i\n", n->dato);
+       n = n->siguiente;
+   }
+}
 
 int main() {
 
@@ -19,12 +62,15 @@ int main() {
     primero->dato = 1;
     primero->siguiente = segundo;
 
-    segundo->dato = 6;
+    segundo->dato = 2;
     segundo->siguiente = tercero;
 
-    tercero->data = 4;
+    tercero->dato = 3;
     tercero->siguiente = NULL;
 
-    printf("Hello, World!\n");
+    printearLista(primero);
+    int pepe[] = {1,3,2,4,1,2,41,5,12,};
+    printf("%i \n", sizeof(pepe)/sizeof(int));
+
     return 0;
 }
