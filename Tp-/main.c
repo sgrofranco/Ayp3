89464cbd307cd045396d5eed6afbcc9a5945786c
Lucas Martin <65137978+Lucasmartining@users.noreply.github.com>
2022-05-31 14:39:50 -0300

    Update main.c

--- Tp-/main.c
+++ Tp-/main.c
@@ -21,54 +21,33 @@ typedef struct Arbol{
 
 ///---------------------------------------------------------
 
-ListaMaterias* inicializarListaMaterias(){
-    ListaMaterias* listaMaterias = (struct ListaMaterias*)malloc(sizeof(struct ListaMaterias));
-    listaMaterias->cabeza = NULL;
-    listaMaterias->cantidadMaterias = 0;
-    return listaMaterias;
-}
 
-ListaAlumnos* inicializarListaAlumnos(){
-    ListaAlumnos* listaAlumnos = (struct ListaAlumnos*) malloc(sizeof (struct ListaAlumnos));
-    listaAlumnos->cantidadAlumnos = 0;
-    listaAlumnos->cabeza = NULL;
-    return listaAlumnos;
-}
 
-Materia* crearMateria(char nombre[]){
 
-    Materia* nuevaMateria = (struct Materia*) malloc(sizeof (struct Materia));
-    nuevaMateria->nombreMateria = nombre;
-    return nuevaMateria;
-};
-
-void agregarMateria(ListaMaterias* lista , char nombre[]){
-    if(lista->cabeza == NULL){
-        lista->cantidadMaterias++;
-        Materia* nuevaMateria = crearMateria(nombre);
-        nuevaMateria->idMateria = lista->cantidadMaterias;
-        lista->cabeza = nuevaMateria;
-    }else{
-        printf("profe te lo corrijo en la prioxima semana \n att la banda de los millones \n");
-    }
-}
-
-void agregarMateriaAAlumno(Alumno* alumno,Materia* matra){
-
-}
 
 int main() {
     ListaMaterias* listaMaterias = inicializarListaMaterias();
-    ListaAlumnos* listaAlumnos = inicializarListaAlumnos();
+    ListaDeAlumnos* listaAlumnos = inicializarListaAlumnos();
     char nombre[] = "pepe";
     char nombreMateria[] = "pepe en ayp3";
-    agregarAlumno(listaAlumnos,nombre,18);
-    agregarMateria(listaMaterias,nombreMateria);
-
-
     char evilnombre[] = "EvilPepe";
+
+    agregarAlumno(listaAlumnos,nombre,18);
     agregarAlumno(listaAlumnos,evilnombre,81);
-    printearLista(listaAlumnos);
+    agregarMateriaAListaDeMaterias(listaMaterias, nombreMateria);
+
+    AnotarseAMateria(listaMaterias,listaAlumnos,1,1);
+    AnotarseAMateria(listaMaterias,listaAlumnos,1,1);
+    AnotarseAMateria(listaMaterias,listaAlumnos,1,2);
+    AnotarseAMateria(listaMaterias,listaAlumnos,1,2);
+
+    printearListaDeAlumnos(listaAlumnos);
+    printf("%s \n", listaAlumnos->cabeza->listaDeHistorialDeMaterias->cabeza->infoMateria->nombreMateria);
+    printf("%s \n", listaAlumnos->cabeza->siguiente->listaDeHistorialDeMaterias->cabeza->infoMateria->nombreMateria);
+    RendirMateria(listaAlumnos,listaMaterias,1,1);
+
+    printf("%i \n", listaAlumnos->cabeza->listaDeHistorialDeMaterias->cabeza->nota);
+
     return 0;
 
 }
