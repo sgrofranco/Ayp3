2d280f781652b0c4266d713db6b3bc92b1844e8a
Lucas Martin <65137978+Lucasmartining@users.noreply.github.com>
2022-05-31 14:41:28 -0300

    Update Materia.c

--- Tp-/Materia.c
+++ Tp-/Materia.c
@@ -4,17 +4,78 @@
 typedef struct Materia{
     int idMateria;
     char *nombreMateria;
-    int nota;
-    struct Materia* materiasCorrelativas[10];
+    struct Materia* siguiente;
 } Materia;
 
-typedef struct HistorialMaterias{
+typedef struct HistorialDeLaMateria{
     int nota; // if nota == Null esta anotado , if nota != Null Rindo bien; si rinde mal se borra.
-    Materia* materia;
-};
+    Materia* infoMateria;
+    struct HistorialDeLaMateria* siguiente;
+} HistorialDeLaMateria;
 
 typedef struct ListaMaterias{
     int cantidadMaterias;
     Materia* cabeza;
 }ListaMaterias;
 
+typedef struct ListaDeHistorialDeMaterias{
+    HistorialDeLaMateria* cabeza;
+} ListaDeHistorialDeMaterias;
+
+Materia* crearMateria(char nombre[]){
+    Materia* nuevaMateria = (struct Materia*) malloc(sizeof (struct Materia));
+    nuevaMateria->nombreMateria = nombre;
+    nuevaMateria->siguiente = NULL;
+    return nuevaMateria;
+};
+
+ListaMaterias* inicializarListaMaterias(){
+    ListaMaterias* listaMaterias = (struct ListaMaterias*)malloc(sizeof(struct ListaMaterias));
+    listaMaterias->cabeza = NULL;
+    listaMaterias->cantidadMaterias = 0;
+    return listaMaterias;
+}
+
+void agregarMateriaAListaDeMaterias(ListaMaterias* lista , char nombre[]){
+    if(lista->cabeza == NULL){
+        lista->cantidadMaterias++;
+        Materia* nuevaMateria = crearMateria(nombre);
+        nuevaMateria->idMateria = lista->cantidadMaterias;
+        lista->cabeza = nuevaMateria;
+    }else{
+        /*
+        Materia* materia = lista->cabeza;
+        while(materia->siguiente){
+            materia = materia->siguiente;
+        }
+        lista->cantidadMaterias++;
+        Materia* nuevaMateria = crearMateria(nombre);
+        nuevaMateria->idMateria = lista->cantidadMaterias;
+        materia->siguiente = nuevaMateria;
+        */
+    }
+}
+
+Materia* getMateria(ListaMaterias* lista, int idMateriaABuscar){
+    Materia* materia = lista->cabeza;
+    while(idMateriaABuscar != materia->idMateria && materia->siguiente){
+        materia = materia->siguiente;
+    }
+    if(idMateriaABuscar == materia->idMateria){
+        return materia;
+    }else{
+        printf("No Existe Materia con esa Id \n");
+    }
+}
+
+HistorialDeLaMateria* CrearHsitorialDeLaMaateria(Materia* materia){
+    HistorialDeLaMateria* historialDeLaMateria = (struct HistorialDeLaMateria*) malloc (sizeof (struct HistorialDeLaMateria));
+    historialDeLaMateria->nota = NULL;
+    historialDeLaMateria->infoMateria = materia;
+    historialDeLaMateria->siguiente = NULL;
+    return historialDeLaMateria;
+}
+
+void destruirHistorialMateria(HistorialDeLaMateria* historialDeLaMateria){
+    free(historialDeLaMateria);
+}
