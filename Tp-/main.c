6d1869a9bdd15cac9cdbfa596ccb55566e8dca55
Lucas Martin <65137978+Lucasmartining@users.noreply.github.com>
2022-06-04 20:59:29 -0300

    Updates En Menu

--- Tp-/main.c
+++ Tp-/main.c
@@ -1,9 +1,59 @@
 #include <stdio.h>
-#include <stdlib.h>
 #import "Alumno.c"
+#import "Utilidades.c"
 #import "Materia.c"
 
-void Menu(){
+void realizarOperacionSolicitada(int eleccionUsuario , ListaMaterias* listaDeMaterias , ListaDeAlumnos* listaDeAlumnos){
+    ///TODO: Switch Case para cara opcion del Menu
+    if(eleccionUsuario == 1){ ///Opcion Inscribir Alumno
+        printf("Ingrese nombre del Alumno \n");
+        char nombreIngresado[30];
+        scanf("%s",&nombreIngresado);
+        printf("%s\n",nombreIngresado);
+        printf("Edad del Alumno? \n");
+        int edadIngresada;
+        scanf("%i",&edadIngresada);
+        ///TODO: Validacion de edad y nombre que no sea vacio
+        agregarAlumno(listaDeAlumnos,nombreIngresado,edadIngresada);
+        printf("El alumno %s fue agregado con Numero de Registro: %li \n",nombreIngresado,listaDeAlumnos->cantidadAlumnos);
+    }
+    if(eleccionUsuario == 2){ ///Opcion Inscribir alumno a una materia
+        int idAlumno = 0;
+        printf("Id Del alumno que desea ingresar a una materia:\n");
+        scanf("%i",&idAlumno);
+        printf("Id de la Materia a la que desea inscribirlo:\n");
+        int idMateria = 0;
+        scanf("%i",&idMateria);
+        ///TODO: Comprobar existencias de Alumno y Materia;
+        AnotarseAMateria(listaDeMaterias,listaDeAlumnos,idMateria,idAlumno);
+    }
+    if(eleccionUsuario == 3){///Buscar Por rango de edad
+        int edadMinima;
+        int edadMaxima;
+        printf("Indique El Rango de edad en el que desea Buscar\n");
+        printf("Edad Minima");
+        scanf("%i",&edadMinima);
+        printf("edad Maxima");
+        scanf("%i",&edadMaxima);
+        MostrarAlumnosPorRangoDeEdad(listaDeAlumnos,edadMinima,edadMaxima);
+    }
+    if(eleccionUsuario == 5){///Rendir Materia
+        printf("Id Del Alumno que desea ingresar la nota \n");
+        long int idAlumno;
+        scanf("%li",&idAlumno);
+        ///TODO: Verificaciones
+        Alumno* alumno = getAlumno(listaDeAlumnos,idAlumno);
+        printearAlumno(alumno);
+        if(puedeRendirMaterias(alumno) != 0){
+            int idMateria;
+            printf("Que Materia quiere rendir? \n");
+            scanf("%i",&idMateria);
+            CargarNota(alumno, getMateria(listaDeMaterias,idMateria));
+        }
+    }
+}
+
+void Menu(ListaMaterias* listaDeMaterias,ListaDeAlumnos* listaDeAlumnos){
     int eleccionUsuario = 1;
     while(eleccionUsuario != 0){
         printf("-------=============-------\n");
@@ -13,21 +63,15 @@ void Menu(){
         printf("2)Inscribir Alumno a una Materia \n");
         printf("3)Mostrar Alumnos por rango de edad \n");
         printf("4)Buscar Alumno por nombre \n");
-        printf("4)Ingresar Resultado de examen \n");
-        printf("5)Ingresar Nueva Materia \n");
+        printf("5)Ingresar Resultado de examen \n");
+        printf("6)Ingresar Nueva Materia \n");
         printf("0)Salir\n");
         scanf("%i", &eleccionUsuario);
+        realizarOperacionSolicitada(eleccionUsuario,listaDeMaterias,listaDeAlumnos);
     }
     ///TODO: Verificacion de que lo ingresado por el usuario este en el menu
 }
 
-
-void realizarOperacionSolicitada(int eleccionUsuario){
-    ///TODO: Switch Case para cara opcion del Menu
-    if(eleccionUsuario == 1){ ///Opcion Inscribir Alumno
-        printf("");
-    }
-}
 
 int main() {
     ListaMaterias* listaMaterias = inicializarListaMaterias();
@@ -46,10 +90,14 @@ int main() {
     agregarMateriaAListaDeMaterias(listaMaterias, "Materia de Langdom");
 
 
-
+    /// ACA ROMPE
     printearListaDeAlumnos(listaAlumnos);
+
+    Menu(listaMaterias,listaAlumnos);
+
     printearListaDeMaterias(listaMaterias);
 
+    /*
     AnotarseAMateria(listaMaterias,listaAlumnos,1,1);
     AnotarseAMateria(listaMaterias,listaAlumnos,1,1);
     AnotarseAMateria(listaMaterias,listaAlumnos,1,2);
@@ -67,8 +115,9 @@ int main() {
     printf("Materia Correlativa: %s \n", listaMaterias->cabeza->siguiente->siguiente->arrayCorrelativas[2]->nombreMateria);
     printf("Materia Correlativa: %s \n", listaMaterias->cabeza->siguiente->siguiente->arrayCorrelativas[3]->nombreMateria);
     printf("Materia Correlativa: %s \n", listaMaterias->cabeza->siguiente->siguiente->arrayCorrelativas[4]->nombreMateria);
+    */
 
-    printearListaDeMaterias(listaMaterias);
+    //printearListaDeMaterias(listaMaterias);
 
     //printf("%i \n", listaAlumnos->cabeza->listaDeHistorialDeMaterias->cabeza->nota);
     //printf("%s \n", listaAlumnos->cabeza->listaDeHistorialDeMaterias->cabeza->infoMateria->nombreMateria);
